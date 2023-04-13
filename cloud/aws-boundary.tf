55015c12db923fc84c1dc69c341a1e1e025408ca
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-04-12 21:05:04 -0400

    Deploy second version of network

--- cloud/aws-boundary.tf
+++ cloud/aws-boundary.tf
@@ -19,3 +19,17 @@ module "boundary" {
   vpc_cidr_block               = module.aws.vpc_cidr_block
   vpc_id                       = module.aws.vpc_id
 }
+
+module "boundary" {
+  source                       = "joatmon08/boundary/aws"
+  version                      = "0.3.1"
+  name                         = "${var.main_project_tag}-boundary-v2"
+  allow_cidr_blocks_to_api     = var.allow_cidr_blocks
+  allow_cidr_blocks_to_workers = var.allow_cidr_blocks
+  boundary_db_username         = var.boundary_db_username
+  boundary_db_password         = random_password.boundary_database.result
+  private_subnet_ids           = module.aws_v2.private_subnets
+  public_subnet_ids            = module.aws_v2.public_subnets
+  vpc_cidr_block               = module.aws_v2.vpc_cidr_block
+  vpc_id                       = module.aws_v2.vpc_id
+}
--- cloud/aws.tf
+++ cloud/aws.tf
@@ -5,3 +5,11 @@ module "aws" {
   main_project_tag = var.main_project_tag
   network_cidr     = var.network_cidr
 }
+
+module "aws_v2" {
+  source           = "app.terraform.io/a-demo-organization/nfd31/aws"
+  version          = "0.0.4"
+  region           = var.region
+  main_project_tag = "${var.main_project_tag}-v2"
+  network_cidr     = "10.128.0.0/20"
+}

cce21e597ae7d100044f70edf92feacff4f6b45f
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-04-13 08:57:44 -0400

    Add second network

--- cloud/aws.tf
+++ cloud/aws.tf
@@ -8,4 +8,13 @@ module "aws" {
 
 locals {
   main_project_tag_v2 = "${var.main_project_tag}-v2"
+  network_cidr_v2     = "10.0.0.0/16"
+}
+
+module "aws_v2" {
+  source           = "app.terraform.io/a-demo-organization/nfd31/aws"
+  version          = "0.0.3"
+  region           = var.region
+  main_project_tag = local.main_project_tag_v2
+  network_cidr     = local.network_cidr
 }

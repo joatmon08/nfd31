48faf24531d00a7032bdb0340dd1de008069d111
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2023-04-12 21:29:32 -0400

    Second network

--- cloud/aws-boundary.tf
+++ cloud/aws-boundary.tf
@@ -6,19 +6,19 @@ resource "random_password" "boundary_database" {
   special     = false
 }
 
-# module "boundary" {
-#   source                       = "joatmon08/boundary/aws"
-#   version                      = "0.3.1"
-#   name                         = "${var.main_project_tag}-boundary"
-#   allow_cidr_blocks_to_api     = var.allow_cidr_blocks
-#   allow_cidr_blocks_to_workers = var.allow_cidr_blocks
-#   boundary_db_username         = var.boundary_db_username
-#   boundary_db_password         = random_password.boundary_database.result
-#   private_subnet_ids           = module.aws.private_subnets
-#   public_subnet_ids            = module.aws.public_subnets
-#   vpc_cidr_block               = module.aws.vpc_cidr_block
-#   vpc_id                       = module.aws.vpc_id
-# }
+module "boundary" {
+  source                       = "joatmon08/boundary/aws"
+  version                      = "0.3.1"
+  name                         = "${var.main_project_tag}-boundary"
+  allow_cidr_blocks_to_api     = var.allow_cidr_blocks
+  allow_cidr_blocks_to_workers = var.allow_cidr_blocks
+  boundary_db_username         = var.boundary_db_username
+  boundary_db_password         = random_password.boundary_database.result
+  private_subnet_ids           = module.aws.private_subnets
+  public_subnet_ids            = module.aws.public_subnets
+  vpc_cidr_block               = module.aws.vpc_cidr_block
+  vpc_id                       = module.aws.vpc_id
+}
 
 module "boundary_v2" {
   source                       = "joatmon08/boundary/aws"
